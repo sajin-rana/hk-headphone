c1f1fcb2c937160b34ec800b5e6b7002cefb8f24
Sajin Rana <132703827+sajin-rana@users.noreply.github.com>
2023-06-21 09:35:04 +0800

    Add files via upload

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import "./Banner.scss";
+
+import BannerImg from "../../../assets/banner-img.png";
+
+const Banner = () => {
+    return <div className="hero-banner">
+        <div className="content">
+            <div className="text-content">
+                <h1>SALES</h1>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and 
+                    typesetting industry. Lorem Ipsum has been the i
+                    ndustry's standard dummy text ever since the 1500s,
+                </p>
+                <div className="ctas">
+                    <div className="banner-cta">Read More</div>
+                    <div className="banner-cta v2">Shop Now</div>
+                </div>
+            </div>
+            <img className="banner-img" src={BannerImg} alt="banner" />
+        </div>
+    </div>;
+};
+
+export default Banner;
